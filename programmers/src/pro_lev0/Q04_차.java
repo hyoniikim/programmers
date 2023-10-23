3bce05a57793ca5b7c2ae4e6a2af2e4c992f5a17
hyoniikim <hx.won10127@gmail.com>
2023-10-23 12:01:39 +0900

    [update] Q04

---
+++ programmers/src/pro_lev0/Q04_차.java
@@ -0,0 +1,22 @@
+package pro_lev0;
+
+public class Q04_차 {
+
+	public static void main(String[] args) {
+		// 두수의 차
+		//정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 solution 함수를 완성해주세요.
+		
+		//입출력 예
+		//num1	num2	result
+		//2		3		-1
+		//100	2		98
+		
+		int num1=100;
+		int num2=2;
+		int answer=num1-num2;
+		
+		System.out.println(answer);
+
+	}
+
+}
