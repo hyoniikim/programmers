218f4f26407a8d4759dd19333a1a7860b0ede58d
hyoniikim <hx.won10127@gmail.com>
2023-10-23 11:43:55 +0900

    [update] Q01

--- programmers/src/pro_lev0/Q01_몫구하기.java
+++ programmers/src/pro_lev0/Q01_몫구하기.java
@@ -1,10 +1,21 @@
 package pro_lev0;
 
-public class Q01 {
+public class Q01_몫구하기 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 몫 구하기
+		//정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
  
+		// 입출력 예
+		// num1		num2	result
+		//	10		 5		  2 
+		// 	7		 2		  3
+		
+		int num1=7;
+		int num2=2;
+		int answer=num1/num2;
+		
+		System.out.println(answer);
 	}
 
 }
