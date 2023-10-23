1f6e900f700267aae03a35bd87ac06e7e4649b8f
hyoniikim <hx.won10127@gmail.com>
2023-10-23 12:04:00 +0900

    [update] Q05

---
+++ programmers/src/pro_lev0/Q05_곱.java
@@ -0,0 +1,22 @@
+package pro_lev0;
+
+public class Q05_곱 {
+
+	public static void main(String[] args) {
+		// 두수의 곱
+		// 정수 num1, num2가 매개변수 주어집니다
+		// num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+		
+		// 입출력 예
+		//num1	num2	result
+		//3		4		12
+		//27	19		513
+		
+		int num1=27;
+		int num2=19;
+		int answer=num1*num2;
+		
+		System.out.println(answer);
+	}
+
+}
