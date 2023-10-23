b61e930c541ad10bcb8fe74d256ecaf8a3d525f8
hyoniikim <hx.won10127@gmail.com>
2023-10-23 11:58:19 +0900

    [update] Q03

---
+++ programmers/src/pro_lev0/Q03_합.java
@@ -0,0 +1,23 @@
+package pro_lev0;
+
+public class Q03_합 {
+
+	public static void main(String[] args) {
+		// 두 수의 합
+		// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+		
+		// 입출력 예
+		//num1		num2	result
+		//2			3		5
+		//100		2		102
+
+		int num1=100;
+		int num2=2;
+		int answer=num1+num2;
+		
+		System.out.println(answer);
+		
+		
+	}
+
+}
