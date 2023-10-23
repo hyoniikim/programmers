ce1fb4f691f4d3e7a456b0d22b114bf3852fbd0b
hyoniikim <hx.won10127@gmail.com>
2023-10-23 12:19:07 +0900

    [update] Q08

---
+++ programmers/src/pro_lev0/Q08_나머지구하기.java
@@ -0,0 +1,23 @@
+package pro_lev0;
+
+public class Q08_나머지구하기 {
+
+	public static void main(String[] args) {
+		// 나머지 구하기
+		// 정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+		
+		//입출력 예
+		//num1	num2	result
+		// 3	 2		  1
+		// 10	 5		  0
+
+		int num1=10;
+		int num2=5;
+		int answer=0;
+		
+		answer=num1%num2;
+		
+		System.out.println(answer);
+	}
+
+}
