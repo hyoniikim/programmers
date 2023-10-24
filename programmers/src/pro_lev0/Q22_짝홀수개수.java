d1ae6fd47af29f33e077d2c4bf051dbb784c40f6
hyoniikim <hx.won10127@gmail.com>
2023-10-24 15:31:50 +0900

    [update] Q22

---
+++ programmers/src/pro_lev0/Q22_짝홀수개수.java
@@ -0,0 +1,33 @@
+package pro_lev0;
+
+import java.util.Arrays;
+
+public class Q22_짝홀수개수 {
+
+	public static void main(String[] args) {
+		// 짝수 홀수 개수
+		// 정수가 담긴 리스트 num_list가 주어질 때, num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+		
+		// 제한사항
+		// 1 ≤ num_list의 길이 ≤ 100
+		// 0 ≤ num_list의 원소 ≤ 1,000
+		
+		// 입출력 예
+		// num_list				result
+		// [1, 2, 3, 4, 5]		[2, 3]
+		// [1, 3, 5, 7]			[0, 4]
+		
+		int[] num_list = {1, 3, 5, 7};
+		int[] answer = {0, 0};
+		
+		for(int i=0; i<num_list.length; i++) {
+			if(num_list[i]%2==0) {
+				answer[0]++;
+			}else if(num_list[i]%2!=0) {
+				answer[1]++;
+			}
+		}
+		System.out.println(Arrays.toString(answer));
+	}
+
+}
