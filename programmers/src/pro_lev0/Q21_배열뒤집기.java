6623180cabe9bb130d2095fb16a96588bb9c465d
hyoniikim <hx.won10127@gmail.com>
2023-10-24 15:23:29 +0900

    [update] Q21

---
+++ programmers/src/pro_lev0/Q21_배열뒤집기.java
@@ -0,0 +1,31 @@
+package pro_lev0;
+
+import java.util.Arrays;
+
+public class Q21_배열뒤집기 {
+
+	public static void main(String[] args) {
+		// 배열 뒤집기
+		// 정수가 들어 있는 배열 num_list가 매개변수로 주어집니다.
+		// num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+		
+		// 제한사항
+		// 1 ≤ num_list의 길이 ≤ 1,000
+		// 0 ≤ num_list의 원소 ≤ 1,000
+		
+		// 입출력 예
+		// num_list						result
+		// [1, 2, 3, 4, 5]				[5, 4, 3, 2, 1]
+		// [1, 1, 1, 1, 1, 2]			[2, 1, 1, 1, 1, 1]
+		// [1, 0, 1, 1, 1, 3, 5]		[5, 3, 1, 1, 1, 0, 1]
+
+		int[] num_list = {1, 0, 1, 1, 1, 3, 5};
+		int[] answer = new int[num_list.length];
+		
+		for(int i=0; i<num_list.length; i++) {
+			answer[i]=num_list[num_list.length-i-1]; //-i-1은 역순
+		}
+		System.out.println(Arrays.toString(answer));
+	}
+
+}
